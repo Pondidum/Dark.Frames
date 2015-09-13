ea25eeb398e5a8621447b0285054fd552f762ded
andy <andy@andydote.co.uk>
2015-09-13 16:28:39 +0100

    system: add drycoded frame builder

---
+++ system/components/health.lua
@@ -0,0 +1,83 @@
+local addon, ns = ...
+
+ns.components.health = ns:create({
+
+  requires = {
+    "root"
+  },
+
+  ctor = function(self)
+    local frame = CreateFrame("StatusBar", "$parentHealth", self.root)
+    local background = frame:CreateTexture(nil, "BORDER")
+    local text = frame:CreateFontString(nil, "OVERLAY")
+
+    frame:SetAllPoints(self.root)
+    background:SetAllPoints(frame)
+    text:SetPoint("RIGHT", frame, "RIGHT", -4, 0)
+
+    --text:SetFont(font, size, style)
+    text:SetShadowColor(0, 0, 0)
+    text:SetShadowOffset(1.25, -1.25)
+
+    self.frame = frame
+    self.background = background
+    self.text = text
+
+  end,
+
+  update = function(self)
+
+    local min, max = 0, UnitMaxHealth(self.unit)
+    local current = UnitHealth(self.unit)
+
+    self:updateBar(min, max, current)
+    self:updateText(min, max, current)
+
+  end,
+
+  updateBar = function(self, min, max, current)
+    self.frame:SetMinMaxValues(min, max)
+    self.frame:SetValue(current)
+  end,
+
+  updateText = function(self, min, max, current)
+
+    local text = self.text
+    local unit = self.unit
+
+    if not UnitIsConnected(unit) then
+      text:SetText("Disconnected")
+
+    elseif UnitIsDead(unit) then
+      text:SetText("Dead")
+
+    elseif UnitIsGhost(unit) then
+      text:SetText("Ghost")
+
+    elseif current ~= max then
+      text:SetText(self:shortValue(current) .. " | " .. floor(current / max * 100) .. "%")
+
+    else
+      text:SetText(self:shortValue(max))
+
+    end
+
+  end,
+
+  shortValue = function(self, v)
+
+    if v == nil then
+      return 0
+    end
+
+    if v >= 1e6 then
+      return ("%.1fm"):format(v / 1e6):gsub("%.?0+([km])$", "%1")
+    elseif v >= 1e3 or v <= -1e3 then
+      return ("%.1fk"):format(v / 1e3):gsub("%.?0+([km])$", "%1")
+    end
+
+    return v
+
+  end
+
+})
---
+++ system/system.lua
@@ -0,0 +1,62 @@
+local addon, ns = ...
+
+ns.components = {
+}
+
+ns.units = {
+  player = {
+    requires = { "root", "health" }
+  },
+}
+
+ns.create = function(self, config)
+  return setmetatable({}, { __index = config })
+end
+
+ns.buildGraph = function(self)
+
+  local isEmpty = function(t)
+    return next(t) == nil
+  end
+
+  local ordered = {}
+  local stack = { "root" }
+
+  while not isEmpty(stack) do
+
+    local currentNode = table.remove(stack)
+    table.insert(ordered, currentNode)
+
+    for otherName, otherNode in pairs(self.components) do
+      if otherNode.requires[currentNode] then
+        otherNode.requires[currentNode] = nil
+        if isEmpty(otherNode.requires) then
+          table.insert(stack, otherName)
+        end
+      end
+    end
+
+  end
+
+  return ordered
+end
+
+ns.build = function(self)
+
+  local components = self:buildGraph()
+
+  for unitName, unit in pairs(self.units) do
+
+    local root = self.components.root:new(unitName)
+
+    for i, name in ipairs(components) do
+
+      if unit.requires[name] then
+        root[name] = self.components[name]:new()
+      end
+
+    end
+
+  end
+
+end
