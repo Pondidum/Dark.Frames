be8c7db097ef826dce938ead5ec5cbde643e7c35
andy <andy@andydote.co.uk>
2015-09-18 22:38:17 +0100

    root: create a frame, have a default size

--- system/components/root.lua
+++ system/components/root.lua
@@ -2,4 +2,8 @@ local addon, ns = ...
 
 ns.components.root = ns:create({
   requires = {},
+  ctor = function(self)
+    self.frame = CreateFrame("Frame", "DarkUnit" .. self.unit, "UIParent")
+    self.frame:SetSize(280, 50)
+  end,
 })
